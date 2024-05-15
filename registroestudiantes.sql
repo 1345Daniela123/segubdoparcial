0a36d4ad6f6354a9cfc5250e086b553ebb0ed917
1345Daniela123 <princesdanijun@gmail.com>
2024-05-14 15:15:11 -1200

    Add files via upload

---
+++ registroestudiantes.sql
@@ -0,0 +1,2 @@
+INSERT INTO `estudiantes`(`id`, `nombre`, `apellido`, `seminario`, `confirmado`, `fecha`) VALUES (1, 'Daniela', 'Huanca Ramirez', 'Redes', 1, '12-11-2023');
+INSERT INTO `estudiantes`(`id`, `nombre`, `apellido`, `seminario`, `confirmado`, `fecha`) VALUES (2, 'Miguel', 'Mamani Cruz', 'Calculo1', 0, '26-01-2024');
